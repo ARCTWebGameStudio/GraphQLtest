c32e9c684a82917349f892e10c4847bb27651ce4
hogeAmiko <hogehoge1722@protonmail.com>
2021-09-23 21:09:16 +0300

    success Schema is work

--- server/index.js
+++ server/index.js
@@ -1,18 +1,78 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
-const schema = require('./schema')
+const { readFileSync } = require('fs')
+const { buildSchema } = require('graphql')
+
+const schemaString = readFileSync('./schema.graphql', { encoding: 'utf8' });
+const schema = buildSchema(schemaString);
+
+
+const allUsers = [
+    {
+        id: '1',
+        username: 'Gunplank',
+        age: '22',
+        post: 'JBP'
+    },
+    {
+        id: '2',
+        username: 'Brand',
+        age: '24',
+        post: 'ANS'
+    },
+    {
+        id: '3',
+        username: 'Warwick',
+        age: '21',
+        post: 'KLP'
+    },
+]
+
+
+
+const root = {
+
+
+    getAllUsers: () => {
+        return allUsers;
+        },
+
+
+    getUser: params => {
+        return allUsers.find(
+            ({ id }) => params.id === id
+        )
+    },
+
+
+    addUser: params => {
+        allUsers.push(
+            {
+            id: allUsers.length + 1,
+            username: params.username,
+            age: params.age,
+            post: params.post
+        }
+        )
+
+        return true;
+    }
+};
+
+
+
 
 const app = express()
 
 app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
-    graphql: true,
-    schema
+    schema: schema,
+    rootValue: root,
+    graphql: true
+
 }))
 
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000')
-})
+app.listen(5000, () => console.log('Server started on port 5000'))
---
+++ server/testo.js
@@ -0,0 +1,90 @@
+let express = require('express');
+let { graphqlHTTP } = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+const allUsers = [
+    {
+        id: '1',
+        username: 'Gunplank',
+        age: '22',
+        post: 'JBP'
+    },
+    {
+        id: '2',
+        username: 'Brand',
+        age: '24',
+        post: 'ANS'
+    },
+    {
+        id: '3',
+        username: 'Warwick',
+        age: '21',
+        post: 'KLP'
+    },
+]
+
+let schema = buildSchema(`
+  type User{
+        id: ID!
+        username: String!
+        age: Int
+        posts: [Post]
+},
+
+    type Post{
+        id: ID
+        title: String
+        content: String
+},
+    input UserInput{
+        id: ID
+        username: String!
+        age: Int!
+        posts: [PostInput]
+},
+
+    input PostInput{
+        id: ID
+        title: String!
+        content: String!
+},
+
+    type Query{
+        getAllUsers: [User]
+        getUser(id: ID): User
+ }
+`);
+
+let root = {
+    getAllUsers: () => {
+        return allUsers;
+    },
+    getUser: params => {
+        return allUsers.find(
+            ({ id }) => params.id === id
+        )
+    },
+
+
+    addUser: params => {
+        allUsers.push(
+            {
+                id: allUsers.length + 1,
+                username: params.username,
+                age: params.age,
+                post: params.post
+            }
+        )
+
+        return true;
+    }
+
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
